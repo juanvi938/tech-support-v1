2f0cc1603918a8c249be6e3fa5ed16b70f1c1910
Juan Vi <juan.vi938@gmail.com>
2014-03-21 12:42:49 +0100

    Modificado metodo start para que se detenga la aplicacion si el texto introducido contiene 'bye' indiferentemente de si es con mayusculas o minusculas

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,6 +38,8 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
+            input = input.trim();
+            input = input.toLowerCase();
 
             if(input.contains("bye")) {
                 finished = true;
